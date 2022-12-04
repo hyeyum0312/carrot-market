15754b7ff276b2383892c463fd3cddaaa9ec17ce
조혜연 <hy_312@mncnext.co.kr>
2022-12-04 18:09:43 +0900

    api 설치

---
+++ carrot-market/pages/api/client-test.tsx
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import client from "../../libs/client";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    await client.user.create({
+        data : {
+            email : 'hi',
+            name : '혜연'
+        }
+    });
+    res.json({
+        ok: true
+    })
+}
