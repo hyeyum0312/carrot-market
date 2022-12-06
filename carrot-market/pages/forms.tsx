04eab91804950e110c2c27b8fd2f9b3c23a4d2e7
조혜연 <hy_312@mncnext.co.kr>
2022-12-07 00:49:22 +0900

    react hook form의 기능들 공부

--- carrot-market/pages/forms.tsx
+++ carrot-market/pages/forms.tsx
@@ -5,31 +5,50 @@ interface LoginForm {
     username : string;
     email : string;
     password : string;
+    error:string;
 }
 
 export default function Forms() {
-    const {register, handleSubmit, formState:{errors}} =  useForm<LoginForm>({
-        mode : "onSubmit"
+    const {
+        register,
+        handleSubmit,
+        watch,
+        setValue,
+        setError,
+        reset, // 모든필드
+        resetField, //특정필드
+        formState:{errors}
+    } =  useForm<LoginForm>({
+        mode : "onSubmit",
     });
     console.log('errors', errors.email?.message);
     const onValid = (data:LoginForm) => {
         console.log('invalid', data);
+        setError('error', {message:'오류가 났습니다.'})
+        setError('username', {message:'사용중인 이름입니다.'})
+        setError('email', {message:'이메일오류입니다.'})
+
     }
     const onInValid = (errors:FieldErrors) => {
         console.log('errors', errors)
+        // reset();
+        resetField('password');
     }
+
+    setValue('username','Hello')
     return <form onSubmit={handleSubmit(onValid, onInValid)}>
         <input {...register("username",{
             required:"이름을 입력하세요",
             minLength:5
         })} type="text" placeholder="Username"/>
+        {errors.error?.message}
         <input {...register("email", {
             required:"이메일을 입력하세요",
             validate : {
                 notGmail : (value) => !value.includes('@gmail.com') ? "":"지메일은 불가."
             }
         })} type="email" placeholder="Email" className={`${Boolean(errors.email) ? "border-red-500":""}`}/>
-        {errors.email?.message}
+        {errors.username?.message}
         <input {...register("password", {
             required:"비밀번호를 입력하세요",
             minLength: {
@@ -38,5 +57,6 @@ export default function Forms() {
             },
         })} type="password" placeholder="Password"/>
         <input type="submit" value="Create Account"/>
+        {errors.error?.message}
     </form>
 }
