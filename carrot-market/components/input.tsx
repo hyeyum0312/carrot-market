db4891530da5674eff59de7be6d5610274efe644
조혜연 <hy_312@mncnext.co.kr>
2022-12-20 22:45:24 +0900

    리팩토링중

--- carrot-market/components/input.tsx
+++ carrot-market/components/input.tsx
@@ -1,14 +1,22 @@
+import {UseFormRegister, UseFormRegisterReturn} from "react-hook-form";
+
 interface InputProps {
     label: string;
     name: string;
     kind?: "text" | "phone" | "price";
+    type: string;
+    register: UseFormRegisterReturn
     [key: string]: any;
+    required: boolean;
 }
 
 export default function Input({
       label,
       name,
       kind = "text",
+      register,
+      type,
+      required,
       ...rest
     }: InputProps) {
     return (
@@ -24,6 +32,7 @@ export default function Input({
                     <input
                         id={name}
                         {...rest}
+                        type={type}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
@@ -36,6 +45,8 @@ export default function Input({
                     <input
                         id={name}
                         {...rest}
+                        type={type}
+                        required={required}
                         className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                     <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -45,12 +56,14 @@ export default function Input({
             ) : null}
             {kind === "phone" ? (
                 <div className="flex rounded-md shadow-sm">
-          <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
-            +82
-          </span>
+                    <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
+                      +82
+                    </span>
                     <input
                         id={name}
                         {...rest}
+                        type={type}
+                        required={required}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
