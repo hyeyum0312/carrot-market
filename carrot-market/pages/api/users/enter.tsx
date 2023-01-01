85fbfee5e494d1f6044a05ebebd7b1b08be8b31d
조혜연 <hy_312@mncnext.co.kr>
2023-01-01 18:34:28 +0900

    z코드수정

--- carrot-market/pages/api/users/enter.tsx
+++ carrot-market/pages/api/users/enter.tsx
@@ -3,16 +3,14 @@ import withHandler from "@libs/server/withHandler";
 import client from "@libs/server/client";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     const {phone, email} = req.body;
-
+    const payload = phone ? {phone: +phone} : {email}
     const user = await client.user.upsert({
         where: {
-            ...(phone ? {phone: +phone}: ''),
-            ...(email ? {email}: ''),
+            ...payload
         },
         create: {
             name: "Anonymous",
-            ...(phone ? {phone: +phone}: ''),
-            ...(email ? {email}: ''),
+            ...payload
         },
         update: {}
     })
