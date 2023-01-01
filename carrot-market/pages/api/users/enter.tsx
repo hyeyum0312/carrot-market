623faf7735674211c766526962d45a5ef3b25a17
조혜연 <hy_312@mncnext.co.kr>
2023-01-01 23:22:37 +0900

    수정

--- carrot-market/pages/api/users/enter.tsx
+++ carrot-market/pages/api/users/enter.tsx
@@ -1,6 +1,8 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import withHandler from "@libs/server/withHandler";
+
 import client from "@libs/server/client";
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     const {phone, email} = req.body;
     const payload = phone ? {phone: +phone} : {email}
